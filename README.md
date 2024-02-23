a21361175b279405a54eaf2f4be24e03c51af67a
Tobias Küster <tobias.kuester@dai-labor.de>
2024-02-23 17:48:27 +0100

    new version with JSON action descriptions like in OPACA API

---
+++ README.md
@@ -0,0 +1,3 @@
+# OpenAI API Test
+
+Simple script for testing how OpenAI API works for finding suitable OPACA actions. Given a prompt with some known OPACA agents and actions in different formats and tries to answer some question about those.
---
+++ openai-test.py
@@ -0,0 +1,105 @@
+import openai
+
+client = openai.OpenAI()
+
+services = """
+getTemperature(location: Location, date: Date): Float;
+  getCurrentLocation(user: string): Location; 
+  getToday(): Date
+"""
+
+
+services = """
+bookDesk(room: Room)
+getAirQuality(room: Room): float
+getListOfRooms(): List<Room>
+showRoute(room: Room)
+"""
+
+services = """
+"agents": [
+    {
+      "agentId": "desk-agent",
+      "agentType": "de.gtarc.opaca.sample.DeskAgent",
+      "actions": [
+        {
+          "name": "GetDesks",
+          "parameters": {
+            "room": "string"
+          },
+          "result": "List[int]"
+        },
+        {
+          "name": "IsFree",
+          "parameters": {
+            "desk": "int"
+          },
+          "result": "bool"
+        },
+        {
+          "name": "BookDesk",
+          "parameters": {
+            "desk": "int"
+          },
+          "result": "bool"
+        }
+      ]
+    },
+    {
+      "agentId": "sensor-agent",
+      "agentType": "de.gtarc.opaca.sample.SensorAgent",
+      "actions": [
+        {
+          "name": "GetTemperature",
+          "parameters": {
+            "room": "string"
+          },
+          "result": "float"
+        },
+        {
+          "name": "GetNoise",
+          "parameters": {
+            "room": "string"
+          },
+          "result": "float"
+        },
+        {
+          "name": "GetHumidity",
+          "parameters": {
+            "room": "string"
+          },
+          "result": "float"
+        }
+      ]
+    },
+    {
+      "agentId": "wayfinding-agent",
+      "agentType": "de.gtarc.opaca.sample.WayfindingAgent",
+      "actions": [
+        {
+          "name": "NavigateTo",
+          "parameters": {
+            "room": "string"
+          },
+          "result": "null"
+        }
+      ]
+    }
+  ]
+"""
+
+
+system = """
+You suggest web services to fulfil a given purpose.
+You present the result as pseudo-code, including temporary variables if needed.
+You know the following agents providing different actions, described in JSON, which can be called as webservices:   
+""" + services
+
+#user = "given my name, what is the temperature in my location?"
+user = "get a list of rooms with temperature lower than 25 degrees, book me a free desk in one of these rooms, and navigate me to that room"
+
+completion = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[{"role": "system", "content": system}, {"role": "user", "content": user}]
+  )
+print(completion.choices[0].message.content)
