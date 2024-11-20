53517cfd5ca623261a042f0f4d80fa09b0679249
Tobias Küster <tobias.kuester@dai-labor.de>
2024-11-20 17:08:44 +0100

    more docs

--- Backend/src/restgpt/__init__.py
+++ Backend/src/restgpt/__init__.py
@@ -1,1 +1,14 @@
+"""
+LLM Client based on RestGPT (https://github.com/Yifan-Song793/RestGPT), but somewhat simplified and adapted to OPACA.
+
+It consists of four different agents:
+* Planner: Responsible for dividing a task into smaller subtasks if necessary. Also handles questions about services
+  or unrelated questions that can be answered without calling actions.
+* API Selector: Based on the output of the Planner, selects a suitable action and generates parameters if necessary.
+* Caller: Parses the output of the API Selector, calls the connected Opaca platform to invoke the specified action 
+  and finally evaluates with call stack to give a short summary in natural language
+* Evaluator: Evaluates the complete plan history and determines if the user query has been fulfilled. If so, generate
+  an answer that will be shown to the user including the achieved results; if not, start over with the Planner.
+"""
+
 from .restgpt_routes import RestGptBackend
--- Backend/src/toolllm/__init__.py
+++ Backend/src/toolllm/__init__.py
@@ -1,1 +1,9 @@
+"""
+LLM Client using LangChain together with the "tools" feature of newer LLM models.
+Does not require "understanding" actions and "parsing" results, and can execute
+multiple tools per iteration, but actions have to fulfill some requirements to
+be translatable to tools (e.g. no cycles in data type definitions).
+Currently only works with GPT-4.
+"""
+
 from .tool_routes import ToolLLMBackend
