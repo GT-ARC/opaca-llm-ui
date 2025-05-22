605b9768d9d644513e6b4e591a15e6865db78445
Oskar Felix Kupke <oskarfelix.kupke@gt-arc.com>
2025-05-22 14:53:17 +0200

    added faq doc file

--- Frontend/src/components/sidebar.vue
+++ Frontend/src/components/sidebar.vue
@@ -437,7 +437,7 @@ export default {
         },
 
         async buildFaqContent() {
-            const readmeUrl = 'https://raw.githubusercontent.com/GT-ARC/opaca-llm-ui/refs/heads/main/README.md';
+            const readmeUrl = 'https://raw.githubusercontent.com/GT-ARC/opaca-llm-ui/refs/heads/main/docs/faq.md';
             try {
                 const response = await fetch(readmeUrl);
                 if (response.ok) {
