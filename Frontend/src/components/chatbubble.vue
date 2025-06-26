4abd2110254df0fd01ae495c0d9863c9cb387281
Cedric Braun <braun@dai-labor.de>
2025-06-26 13:01:16 +0200

    adjust indentation

--- Frontend/src/components/chatbubble.vue
+++ Frontend/src/components/chatbubble.vue
@@ -222,30 +222,29 @@ export default {
         },
 
         getFormattedContent() {
-          try {
-            const rawHtml = marked.parse(this.content);
+            try {
+                const rawHtml = marked.parse(this.content);
 
-            // Load into a temporary DOM element
-            const div = document.createElement('div');
-            div.innerHTML = rawHtml;
+                // Load into a temporary DOM element
+                const div = document.createElement('div');
+                div.innerHTML = rawHtml;
 
-            // Make sure links open in new tab
-            div.querySelectorAll('a').forEach(link => {
-              link.setAttribute('target', '_blank');
-              link.setAttribute('rel', 'noopener noreferrer');
-            });
+                // Make sure links open in new tab
+                div.querySelectorAll('a').forEach(link => {
+                    link.setAttribute('target', '_blank');
+                    link.setAttribute('rel', 'noopener noreferrer');
+                });
 
-            // Sanitize html
-            const safeHtml = DOMPurify.sanitize(div.innerHTML, {
-              // Keep attributes we set
-              ADD_ATTR: ['target', 'rel'],
-            });
-
-            return safeHtml;
-          } catch (error) {
-            console.error('Failed to parse chat bubble content:', this.content, error);
-            return this.content;
-          }
+                // Sanitize html
+                const safeHtml = DOMPurify.sanitize(div.innerHTML, {
+                    // Keep attributes we set
+                    ADD_ATTR: ['target', 'rel'],
+                });
+                return safeHtml;
+            } catch (error) {
+                console.error('Failed to parse chat bubble content:', this.content, error);
+                return this.content;
+            }
         },
 
         setContent(newContent) {
