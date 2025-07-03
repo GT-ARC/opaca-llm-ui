1df43ba635b7c41968c26ab0a92054cce9103028
Cedric Braun <braun@dai-labor.de>
2025-07-03 12:23:56 +0200

    fix merge error

--- Frontend/src/components/content.vue
+++ Frontend/src/components/content.vue
@@ -481,7 +481,6 @@ export default {
         this.loadHistoryOrWelcomeMessage();
 
         this.updateScrollbarThumb();
-        this.showWelcomeMessage();
     },
     watch: {
         textInput() {
