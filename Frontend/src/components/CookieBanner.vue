0929d235b4452fe3df261259cf6f462b08c17414
Tobias Küster <tobias.kuester@dai-labor.de>
2025-07-25 11:58:41 +0200

    style

--- Frontend/src/components/CookieBanner.vue
+++ Frontend/src/components/CookieBanner.vue
@@ -1,13 +1,13 @@
 <template>
     <div v-if="!isCookieAccepted" class="cookie-banner">
         <p>
-            This website uses a cookies to associate your message history and settings (stored in the backend) with you.
-            Without those cookies, no continued conversation with the LLM is possible. You can accept to either accept the cookie
+            This website uses cookies to associate your message history and settings (stored in the backend) with you.
+            Without those cookies, no continued conversation with the LLM is possible. You can chose to either accept the cookies
             for only this browser session (a new session-cookie will be created for each session), or permanently (your message 
             history can be restored if you restart the browser). You can always check, change or revoke this in your browser settings.
         </p>
-        <button @click="acceptCookies(false)">Accept for this Session</button>
-        <button @click="acceptCookies(true)">Accept Persistently</button>
+        <button class="btn btn-secondary" @click="acceptCookies(false)">Accept for this Session</button>
+        <button class="btn btn-primary" @click="acceptCookies(true)">Accept Persistently</button>
     </div>
 </template>
 
@@ -31,9 +31,10 @@ export default {
         },
 
         acceptCookies(persistentSession) {
-            document.cookie = "cookieConsent=true; max-age=" + 60 * 60 * 24 * 30; // 30 days
+            const max_age = 60 * 60 * 24 * 30; // 30 days
+            document.cookie = "cookieConsent=true; max-age=" + max_age;
             if (persistentSession) {
-                document.cookie = "persistentSession=true; max-age=" + 60 * 60 * 24 * 30; // 30 days
+                document.cookie = "persistentSession=true; max-age=" + max_age;
             }
             this.isCookieAccepted = true;
         },
@@ -43,6 +44,16 @@ export default {
 
 <style>
 .cookie-banner {
-
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    padding: 1em;
+    margin: 1em;
+    border-radius: 1em;
+    text-align: center;
+    color: white;
+    background-color: rgba(0, 0, 0, 0.5); /* Transparent overlay */
+    z-index: 9999; /* Should appear above all other items */
 }
 </style>
