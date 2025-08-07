8b416a8cee8e47e7d2fa4b569ecfad23a45902ce
Oskar Felix Kupke <oskarfelix.kupke@gt-arc.com>
2025-08-07 15:48:45 +0200

    more chat spacing adjustments

--- Frontend/src/components/chatbubble.vue
+++ Frontend/src/components/chatbubble.vue
@@ -2,9 +2,9 @@
 
     <!-- user bubble -->
     <div v-if="this.isUser" :id="this.elementId"
-         class="d-flex flex-row justify-content-end mb-4">
+         class="d-flex flex-row justify-content-end">
 
-        <div class="chatbubble chatbubble-user me-2 p-3 mb-2 w-auto ms-auto">
+        <div class="chatbubble chatbubble-user me-2 w-auto">
             <div v-html="this.getFormattedContent()"></div>
         </div>
     </div>
@@ -12,9 +12,9 @@
 
     <!-- ai bubble -->
     <div v-else :id="this.elementId"
-         class="d-flex flex-row justify-content-start mb-4 w-100">
+         class="d-flex flex-row justify-content-start w-100">
 
-        <div class="chatbubble chatbubble-ai me-auto ms-2 p-3 mb-2"
+        <div class="chatbubble chatbubble-ai ms-2"
              :class="{glow: this.isLoading}" :style="this.getGlowColors()">
 
             <div class="d-flex justify-content-start">
@@ -180,8 +180,11 @@ export default {
 
         scrollDownDebugMsg() {
             if (!this.autoScrollDebugMessage) return;
-            const configContainer = document.getElementById(`debug-message-${this.elementId}`);
-            configContainer.scrollTop = configContainer.scrollHeight;
+            const debugId = `debug-message-${this.elementId}`;
+            const debug = document.getElementById(debugId);
+            if (debug) {
+                debug.scrollTop = debug.scrollHeight;
+            }
         },
 
         handleDebugScroll() {
@@ -362,6 +365,7 @@ export default {
     position: relative;
     transition: all 0.2s ease;
     width: fit-content;
+    margin-bottom: 1rem;
 }
 
 .chatbubble-user {
@@ -380,6 +384,7 @@ export default {
     width: 100%;
     will-change: box-shadow;
     transition: box-shadow 0.2s ease;
+    padding: 0.5rem;
 }
 
 .message-text {
