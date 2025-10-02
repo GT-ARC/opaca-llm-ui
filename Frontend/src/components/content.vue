81446e01a57168aa984dc84cc59de69e51a2f8c0
Cedric Braun <braun@dai-labor.de>
2025-10-02 10:55:17 +0200

    handle files being pasted

--- Frontend/src/components/content.vue
+++ Frontend/src/components/content.vue
@@ -716,6 +716,28 @@ export default {
             await this.scrollToMessage(messageId);
         },
 
+        async handlePaste(event) {
+            const items = event.clipboardData.items;
+
+            const files = [];
+
+            for (let i = 0; i < items.length; i++) {
+                const item = items[i];
+                if (item.kind === "file") {
+                    const file = item.getAsFile();
+                    if (file) files.push(file);
+                }
+            }
+
+            if (files.length > 0) {
+                // Prevent from being inserted into the input
+                event.preventDefault();
+
+                await this.uploadFiles(files);
+            }
+            // If no file found, let normal paste happen
+        }
+
     },
 
     mounted() {
