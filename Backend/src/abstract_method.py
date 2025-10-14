269e04062e23901daef93868bcd2ed16b64deddb
Tobias Küster <tobias.kuester@dai-labor.de>
2025-10-14 17:11:44 +0200

    made validate-config-schema a class-method of abstract agent

--- Backend/src/abstract_method.py
+++ Backend/src/abstract_method.py
@@ -268,6 +268,54 @@ class AbstractMethod(ABC):
         return res
 
 
+    @classmethod
+    def validate_config_input(cls, values: Dict[str, Any]):
+        """
+        Validates the given input values against the Configuration Schema
+        :param values: The input dict of configuration parameters that was sent by the UI
+        :return: Returns nothing if everything was successfully validated, raises Exception otherwise
+        """
+        schema = cls.config_schema()
+
+        # Check if all required parameters have been provided
+        if not all(key in values.keys() for key in schema.keys() if schema[key].required):
+            raise ValueError(f'There are required configuration parameters missing!\n'
+                            f'Expected: {[key for key in schema.keys() if schema[key].required]}\n'
+                            f'Received: {[key for key in values.keys()]}')
+
+        for key, value in values.items():
+            # Check if key exist in schema
+            if key not in schema.keys():
+                raise ValueError(f'No option named "{key}" was found!')
+
+            # Make config parameter a dict for easier checks of optional fields
+            if isinstance(schema[key], ConfigParameter):
+                config_param = schema[key].model_dump()
+            else:
+                config_param = schema[key]
+
+            # Validate type consistency
+            if (config_param["type"] == "number" and not isinstance(value, (float, int))) or \
+                (config_param["type"] == "integer" and not isinstance(value, int)) or \
+                (config_param["type"] == "string" and not isinstance(value, str)) or \
+                (config_param["type"] == "boolean" and not isinstance(value, bool)):
+                raise TypeError(f"Parameter '{key}' does not match the expected type '{config_param['type']}'")
+            elif config_param["type"] == "null" and value is not None:
+                raise TypeError(f"Parameter '{key}' does not match the expected type '{config_param['type']}'")
+
+            # Validate min/max limit
+            if config_param["type"] in ["number", "integer"]:
+                if config_param.get("minimum", None) is not None and value < config_param.get("minimum"):
+                    raise ValueError(f"Parameter '{key}' cannot be smaller than its allowed minimum ({config_param['minimum']})")
+                if config_param.get("maximum", None) is not None and value > config_param.get("maximum"):
+                    raise ValueError(f"Parameter '{key}' cannot be larger than its allowed maximum ({config_param['maximum']})")
+
+            # Validate enum
+            if config_param.get("enum", None) and value not in schema[key].enum and not schema[key].free_input:
+                raise ValueError(f"Parameter '{key}' has to be one of {schema[key].enum}")
+
+
+
 
 def openapi_to_functions(openapi_spec, agent: str | None = None, strict: bool = False):
     """
--- Backend/src/utils.py
+++ Backend/src/utils.py
@@ -1,12 +1,7 @@
 import logging
-from typing import Dict, Any
-
-
-from .models import ConfigParameter
 
 
 logger = logging.getLogger(__name__)
-
 
 
 def enforce_strictness(schema):
@@ -28,50 +23,3 @@ def enforce_strictness(schema):
     elif isinstance(schema, list):
         for item in schema:
             enforce_strictness(item)
-
-
-def validate_config_input(values: Dict[str, Any], schema: Dict[str, ConfigParameter]):
-    """
-    Validates the given input values against the Configuration Schema
-    :param values: The input dict of configuration parameters that was sent by the UI
-    :param schema: The schema of the selected prompting method that the parameters should affect
-    :return: Returns true if everything was successfully validated, false otherwise
-    """
-
-    # Check if all required parameters have been provided
-    if not all(key in values.keys() for key in schema.keys() if schema[key].required):
-        raise ValueError(f'There are required configuration parameters missing!\n'
-                         f'Expected: {[key for key in schema.keys() if schema[key].required]}\n'
-                         f'Received: {[key for key in values.keys()]}')
-
-    for key, value in values.items():
-        # Check if key exist in schema
-        if key not in schema.keys():
-            raise ValueError(f'No option named "{key}" was found!')
-
-        # Make config parameter a dict for easier checks of optional fields
-        if isinstance(schema[key], ConfigParameter):
-            config_param = schema[key].model_dump()
-        else:
-            config_param = schema[key]
-
-        # Validate type consistency
-        if (config_param["type"] == "number" and not isinstance(value, (float, int))) or \
-            (config_param["type"] == "integer" and not isinstance(value, int)) or \
-            (config_param["type"] == "string" and not isinstance(value, str)) or \
-            (config_param["type"] == "boolean" and not isinstance(value, bool)):
-            raise TypeError(f"Parameter '{key}' does not match the expected type '{config_param['type']}'")
-        elif config_param["type"] == "null" and value is not None:
-            raise TypeError(f"Parameter '{key}' does not match the expected type '{config_param['type']}'")
-
-        # Validate min/max limit
-        if config_param["type"] in ["number", "integer"]:
-            if config_param.get("minimum", None) is not None and value < config_param.get("minimum"):
-                raise ValueError(f"Parameter '{key}' cannot be smaller than its allowed minimum ({config_param['minimum']})")
-            if config_param.get("maximum", None) is not None and value > config_param.get("maximum"):
-                raise ValueError(f"Parameter '{key}' cannot be larger than its allowed maximum ({config_param['maximum']})")
-
-        # Validate enum
-        if config_param.get("enum", None) and value not in schema[key].enum and not schema[key].free_input:
-            raise ValueError(f"Parameter '{key}' has to be one of {schema[key].enum}")
-
