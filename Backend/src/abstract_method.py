4bd03528955fb4686085791974eb0dcd410e3bd6
Strehlow, Robert <Robert.Strehlow@gt-arc.com>
2025-11-06 13:43:29 +0100

    Switch `logged_in_containers` to set and call logout directly after successful login

--- Backend/src/abstract_method.py
+++ Backend/src/abstract_method.py
@@ -238,13 +238,10 @@ class AbstractMethod(ABC):
                                 result=f"Failed to invoke tool.\nNo credentials provided.")
 
             # Send credentials to container via OPACA
-            await self.session.opaca_client.container_login(response.username, response.password, container_id)
+            await self.session.opaca_client.container_login(container_id, response.username, response.password, response.timeout)
 
         # try to invoke the tool again
         res = await self.invoke_tool(tool_name, tool_args, tool_id, True)
-
-        # auto-logout after some time
-        asyncio.create_task(self.session.opaca_client.deferred_container_logout(container_id, response.timeout))
 
         return res
 
