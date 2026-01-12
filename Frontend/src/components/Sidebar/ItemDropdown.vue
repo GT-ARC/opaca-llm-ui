7ef235e7370cfd4824fa4b655235cc5db246d47e
Oskar Felix Kupke <oskarfelix.kupke@gt-arc.com>
2026-01-12 21:24:41 +0100

    barebones dropdown menu

---
+++ Frontend/src/components/Sidebar/ItemDropdown.vue
@@ -0,0 +1,48 @@
+<template>
+<div v-if="this.visible" ref="root">
+
+    <div v-for="item in this.items"
+         class="item"
+         :key="item.id"
+         @click="item.callback()" >
+        <i class="fa fa-landmark-flag" />
+        {{ item.label }}
+    </div>
+
+</div>
+</template>
+
+<script>
+import {nextTick} from "vue";
+
+export default {
+    name: "ItemDropdown",
+    props: {
+        items: Array,
+    },
+    data() {
+        return {
+            visible: false,
+        }
+    },
+    methods: {
+        async toggle(value = null) {
+            this.visible = value === null
+                ? !this.visible
+                : value;
+        },
+    }
+}
+</script>
+
+<style scoped>
+.item {
+    cursor: pointer;
+    padding: 0.5rem;
+    border-radius: 0.25rem;
+}
+
+.item:hover {
+    color: var(--primary-color);
+}
+</style>
