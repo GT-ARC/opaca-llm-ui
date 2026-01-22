84c1389ed24b6424b125183463445da22e9a8685
Strehlow, Robert <Robert.Strehlow@gt-arc.com>
2026-01-22 12:49:28 +0100

    Simplify general prompt

--- Backend/src/prompts.py
+++ Backend/src/prompts.py
@@ -10,32 +10,12 @@ from datetime import datetime as dt
 import os
 
 
-GENERAL_CAPABILITIES = """
-Besides the available tools and actions, you can use different basic LLM-capabilities,
-such as summarizing or translating texts, or reading files provided in your input.
-The output of the final agent in the chain will be rendered as Markdown, and so also
-allows to e.g. embed images or links to external resources, but no iFrames or JS.
-"""
-
-
-SELF_INTRODUCTION_AND_CAPABILITIES = f"""
-You are 'SAGE', an LLM integrated with the 'OPACA' framework,
-providing access to different agents and their actions as tools.
-
-You can help users in various ways, such as Task Automation, Data Analysis,
-Upskilling, and Working in an Ergonomic way. In what ways exactly depends
-on the tools that are available to you.
-
-{GENERAL_CAPABILITIES}
-
-You are part of a larger group of agents, and may have specific tasks and
-responsibilities. Use the above as context, but always prioritize your specific
-instructions, listed in the following.
-"""
-
-
 def build_full_prompt(specific_prompt: str) -> str:
-    return "\n".join((get_time_and_location(), SELF_INTRODUCTION_AND_CAPABILITIES, specific_prompt))
+    SELF_INTRODUCTION_AND_CAPABILITIES = f"""
+    You are part of an LLM Assistant called \"SAGE\". {get_time_and_location()} Following are your 
+    individual tasks:
+    """
+    return "\n".join((SELF_INTRODUCTION_AND_CAPABILITIES, specific_prompt))
     
 
 def get_time_and_location():
@@ -43,6 +23,6 @@ def get_time_and_location():
     now = dt.strftime(dt.now(), "%B %d %Y, %H:%M")
     loc = os.getenv("LOCATION")
     if loc:
-        return f"The current date and time is {now}. You are located at {loc}"
+        return f"The current date and time is {now}. You are located at {loc}."
     else:
         return f"The current date and time is {now}."
