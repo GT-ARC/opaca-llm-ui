dbc3b3a82de60a2ce6ab534a5b8d89607b5d0d4a
Tobias Küster <tobias.kuester@dai-labor.de>
2026-02-13 14:53:17 +0100

    handle and forward exception from opaca (still has to be handled better in UI)

--- Backend/src/opaca_client.py
+++ Backend/src/opaca_client.py
@@ -7,6 +7,7 @@ import requests
 import httpx
 import jsonref
 from typing import Optional, List, Dict, Any
+from fastapi import HTTPException
 
 logger = logging.getLogger(__name__)
 
@@ -136,7 +137,10 @@ class OpacaClient:
         agent = f"/{agent}" if agent else ""
         async with httpx.AsyncClient() as client:
             res = await client.post(f"{self.url}/invoke/{action}{agent}", json=params, headers=self._headers(), timeout=None)
-        res.raise_for_status()
+        try:
+            res.raise_for_status()
+        except Exception as e:
+            raise HTTPException(status_code=502, detail=res.text)
         return res.json()
 
     async def container_login(self, container_id: str, username: str, password: str):
