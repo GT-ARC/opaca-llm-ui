5b36658e4f78889733e1f6094f5df7efc4ce55b1
Tobias Küster <tobias.kuester@dai-labor.de>
2026-03-30 16:18:38 +0200

    merged what's left of the code to a single module, no other changes

--- Backend/src/code_execution/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Code execution module with sandboxed Python execution."""
-
-from .executor import CodeExecutor
-from .prompts import PYODIDE_CODE_PROMPT, PYODIDE_CODE_RETRY_PROMPT
-from .util import extract_code_block
